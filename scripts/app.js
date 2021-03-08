2a18caa2ce8f2030f579d4865a4d31c37a5d732d
Yathaartha <maharjanyathaartha017@gmail.com>
2021-03-08 20:59:53 +0545

    initialized repo

---
+++ scripts/app.js
@@ -0,0 +1,9 @@
+const submitBtn = document.getElementById("submitBtn");
+
+const correctList = [];
+const incorrectList = [];
+const totalList = [];
+
+function submitBtnHandler() {}
+
+submitBtn.addEventListener("click", submitBtnHandler);
