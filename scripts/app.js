73e10d7eda6771e2db1c59e0119efc7fe58ad307
Yathaartha <maharjanyathaartha017@gmail.com>
2021-03-09 11:52:29 +0545

    added logic to calculate score

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,18 @@
 const submitBtn = document.getElementById("submitBtn");
 
-const correctList = [];
-const incorrectList = [];
-const totalList = [];
+function submitBtnHandler() {
+  let mrkList = document.querySelectorAll('input[type="checkbox"]:checked');
+  let correctCount = 0;
+  for (i = 0; i < mrkList.length; i++) {
+    if (mrkList[i].className == "correct") {
+      correctCount++;
+    }
+  }
+  calculateScore(correctCount);
+}
 
-function submitBtnHandler() {}
+function calculateScore(correctCount) {
+  console.log("Your Score is " + correctCount);
+}
 
 submitBtn.addEventListener("click", submitBtnHandler);
